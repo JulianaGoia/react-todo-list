182112d9a35a60665dcb390e00b67d2f4bece6b0
Juliana <juliana.goia@allycode.com.br>
2023-01-11 19:17:43 -0300

    construção do componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import './global.css';
+import styles from './App.module.css';
+import { Header} from './components/Header';
+// import { TaskList } from './components/TaskList';
 
-
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <h1>resct-todo-list</h1>
+    <div>
+      <Header/>
+      <div className={styles.content}>
+        {/* <TaskList/> */}
+      </div>
     </div>
   )
 }
-
-export default App
