7695a02843fa022052b72418b5f3ab8bbf1e118c
Juliana <juliana.goia@allycode.com.br>
2023-01-12 12:26:37 -0300

    adicionar tarefas na lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './global.css';
 import styles from './App.module.css';
 import { Header} from './components/Header';
-// import { TaskList } from './components/TaskList';
+import { TaskList } from './components/TaskList';
 
 export function App() {
   return (
     <div>
       <Header/>
       <div className={styles.content}>
-        {/* <TaskList/> */}
+        <TaskList/>
       </div>
     </div>
   )
